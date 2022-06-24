29e5949dc7a814fdfb438db101bb210648fda286
aech12 <aech-12@hotmail.com>
2022-06-24 10:48:07 -0300

    added manrope font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./templates/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans]
+      }
+		},
 	},
 	plugins: [],
 };
